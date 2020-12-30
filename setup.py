2125bc2e5e11593b51671dc692c193f098df2041
Olivier Vincent <olivier.vincent@univ-lyon1.fr>
2020-12-30 19:33:02 +0100

    Make version automatically detected from git tag using setuptools_scm

--- setup.py
+++ setup.py
@@ -1,23 +1,4 @@
-from setuptools import setup, find_packages
-import filo
+from setuptools import setup
 
-with open("README.md", "r") as f:
-    long_description = f.read()
-
-setup(
-        name='filo',
-        version=filo.__version__,
-        author='Olivier Vincent',
-        author_email='olivier.vincent@univ-lyon1.fr',
-        url='https://cameleon.univ-lyon1.fr/ovincent/filo',
-        description='Managing files.',
-        long_description=long_description,
-        long_description_content_type="text/markdown",
-        packages=find_packages(),
-        classifiers=[
-            "Programming Language :: Python :: 3",
-            "License :: OSI Approved :: BSD License",
-            "Operating System :: OS Independent",
-        ],
-        python_requires='>=3'
-)
+if __name__ == '__main__':
+    setup(use_scm_version=True)
