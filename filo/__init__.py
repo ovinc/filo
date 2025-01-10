c05968d80ca70f7f59d967f2568b9175ac6b73a4
Olivier Vincent <ovinc.py@gmail.com>
2025-01-10 19:57:49 +0100

    Solve bug of load_csv() not being imported

--- filo/__init__.py
+++ filo/__init__.py
@@ -1,7 +1,8 @@
 """File Management."""
 
 from .general import list_files, list_all, move_files, move_all
-from .general import batch_file_rename, data_to_line, line_to_data
+from .general import batch_file_rename
+from .general import load_csv, data_to_line, line_to_data
 from .general import make_iterable
 
 from .series import Series
