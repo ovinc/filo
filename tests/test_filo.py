c58d8a3c9083aa5cfae78f3414ad3e6b5f4aba2e
Olivier Vincent <ovinc.py@gmail.com>
2025-03-29 20:39:10 +0100

    Make all tests pass

--- tests/test_filo.py
+++ tests/test_filo.py
@@ -4,40 +4,34 @@ import filo
 from pathlib import Path
 from filo import FileSeries
 
-module_path = Path(filo.__file__).parent / '..'
-data_path = module_path / 'data'
+MODULE_PATH = Path(filo.__file__).parent / '..'
+DATA_PATH = MODULE_PATH / 'data'
+FILE_INFO = DATA_PATH / 'External_File_Info.txt'
+TIME_INFO = DATA_PATH / 'External_Time_Info.txt'
 
-folders = data_path / 'img1', data_path / 'img2'
-series = FileSeries(folders, savepath=data_path, extension='.png')
+FOLDERS = DATA_PATH / 'img1', DATA_PATH / 'img2'
+FILES = FileSeries.auto(folders=FOLDERS, refpath=DATA_PATH,  extension='.png')
 
 
 def test_series_numbering():
-    """Verify numbering of files is ok in multiple folders for series."""
-    assert series.files[-1].num == 19
+    """Verify numbering of files is ok in multiple folders for files."""
+    assert FILES[-1].num == 19
 
 
 def test_series_info():
     """test generation of infos DataFrame."""
-    series.load_info('External_File_Info.txt')
-    info = series.info
-    assert round(info.at[4, 'time (unix)']) == 1599832405
-
-
-def test_series_info_overwrite():
-    """Test loading file data from external file."""
-    series.load_info('External_File_Info.txt')
-    info = series.info
-    assert info.at[2, 'time (unix)'] == 1599832401
+    files = FileSeries.from_csv(FILE_INFO, sep='\t', refpath=DATA_PATH)
+    assert round(files.info.at[4, 'time (unix)']) == 1599832405
 
 
 def test_series_info_update_time():
     """Test loading file data from external file."""
-    series.load_time('External_Time_Info.txt')
-    info = series.info
+    FILES.update_times(TIME_INFO)
+    info = FILES.info
     assert info.at[2, 'time (unix)'] == 1607500504
 
 
 def test_series_duration():
-    """Test calculation of time duration of series."""
-    series.load_time('External_Time_Info.txt')
-    assert round(series.duration.total_seconds()) == 38
+    """Test calculation of time duration of files."""
+    FILES.update_times(TIME_INFO)
+    assert round(FILES.duration.total_seconds()) == 38
